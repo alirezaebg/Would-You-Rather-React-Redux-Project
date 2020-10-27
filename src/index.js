4803644ba15fe0ce9e99a5cd27932d961831a6af
Alireza Ebadighajari <alirezaebg@gmail.com>
2020-10-26 21:42:19 -0700

    Add middleware code

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './components/App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { reducer } from './reducers'
-import { middleware } from './middleware'
+import reducer from './reducers'
+import middleware from './middleware'
 
 const store = createStore(reducer, middleware)
 
