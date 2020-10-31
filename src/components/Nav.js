7b030bf2fb7837481bdd705526b686be8569a7c7
Alireza Ebadighajari <alirezaebg@gmail.com>
2020-10-31 09:22:36 -0700

    Logout button function

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,41 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPowerOff } from '@fortawesome/free-solid-svg-icons'
+import { setAuthedUser } from '../actions/authedUser'
 
-function Nav(props) {
-    const { authedUser } = props
-    return (
-        <nav className='nav'>
-            <ul>
-                <li>
-                    <NavLink to='/' exact >
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/new' >
-                        New Poll
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/leaderboard' >
-                        Leader Board
-                    </NavLink>
-                </li>
-            </ul>
-            <div>
-                <p>
-                    Hello, {authedUser}
-                </p>
-                <button>
-                    <FontAwesomeIcon className='power' icon={faPowerOff} />
-                </button>
-            </div>
+class Nav extends Component {
 
-        </nav>
-    )
+    handleClick = (e) => {
+        e.preventDefault()
+
+        const { dispatch } = this.props
+        dispatch(setAuthedUser(null))
+
+    }
+
+    render() {
+        const { authedUser } = this.props
+        return (
+            <nav className='nav'>
+                <ul>
+                    <li>
+                        <NavLink to='/' exact >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/new' >
+                            New Poll
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/leaderboard' >
+                            Leader Board
+                        </NavLink>
+                    </li>
+                </ul>
+                <div>
+                    <p>
+                        Hello, {authedUser}
+                    </p>
+                    <button onClick={this.handleClick}>
+                        <FontAwesomeIcon className='power' icon={faPowerOff} />
+                    </button>
+                </div>
+            </nav>
+        )
+    }
+
 }
 
 function mapStateToProps({ authedUser }) {
